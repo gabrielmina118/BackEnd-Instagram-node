a8a3138bbb43f3474be8893767b2e63ca13ac6fb
gabrielmina118 <gabrielmina118@gmail.com>
2022-01-05 14:46:13 -0300

    cors

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ module.exports = function ({ app, dbConn, upload }) {
     } else {
       const avatar = `/${file.filename}`;
       const { id, email, password, fullname } = req.body;
+      
       if (email && password && fullname) {
         const findAccountByEmail = "SELECT * FROM user_account WHERE user_email = ?";
         dbConn.query(findAccountByEmail, [email], function (error, account) {
@@ -15,6 +16,7 @@ module.exports = function ({ app, dbConn, upload }) {
             res.status(200).jsonp({ message: 'The email existed in the system' });
           } else {
             const users = [[id, email, password, fullname, avatar]];
+            console.log("users",users)
             const registerUserSql = "INSERT INTO user_account (id, user_email, user_password, user_full_name, user_avatar) VALUES ?";
             dbConn.query(registerUserSql, [users], function (error, insertedUser) {
               if (insertedUser) {
