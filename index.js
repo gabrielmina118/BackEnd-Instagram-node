d7c745e5941e757c3dd127ad724b35fc5dc10134
gabrielmina118 <gabrielmina118@gmail.com>
2022-01-05 14:27:32 -0300

    cors

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // config multers.
